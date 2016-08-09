40078b6e88a6a593d7b023dd5130526a1fde5436
Deborah Etsenake <debbyIsAwesome@uws111694usr.uws.ualberta.ca>
2016-08-09 09:02:51 -0600

    chat log file is now deleted when the page is reloaded

--- public/js/endSession_keyboard_shortcuts.js
+++ public/js/endSession_keyboard_shortcuts.js
@@ -4,11 +4,16 @@ $(window).on('mouseover', (function () {
 $(window).on('mouseout', (function () {
     window.onbeforeunload = deleteFile;
 }));
+//deletes chat_log file on reload
+$(window).bind('beforeunload',function(){
+    window.onunload = deleteFile;
+});
 
 var prevKey="";
 $(document).keydown(function (e) {
     if (e.key=="F5") {
         window.onbeforeunload = deleteFile;
+        //window.onunload = deleteFile;
     }
     else if (e.key.toUpperCase() == "W" && prevKey == "CONTROL") {
         window.onbeforeunload = deleteFile;
