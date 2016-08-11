acc9c04b25abfe9b385bd262d61725c87d5ab649
Deborah Etsenake <debbyIsAwesome@uws149409usr.uws.ualberta.ca>
2016-08-11 10:30:17 -0600

    added new function to remove some lines from the file

--- public/js/file_manager.js
+++ public/js/file_manager.js
@@ -11,3 +11,10 @@ function deleteFile() {
   xhr.open( 'post', '/deleteFile', true );
   xhr.send(data);
 }
+
+function editFile() {
+  var data = new XMLHttpRequest();
+  var xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new activeXObject("Microsoft.XMLHTTP");
+  xhr.open( 'post', '/editFile', true );
+  xhr.send(data);
+}
